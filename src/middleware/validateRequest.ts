f38f7563c2f7925653a6af867ebb34ce75805bf7
AndresParraR <johnarlinton@gmail.com>
2022-06-14 10:00:56 -0500

    Message validate Json

--- src/middleware/validateRequest.ts
+++ src/middleware/validateRequest.ts
@@ -12,7 +12,7 @@ const validate =
       });
       next();
     } catch (e) {
-      return res.status(400).send((e as Error).message);
+      return res.status(400).send({message: (e as Error).message});
     }
   };
 
